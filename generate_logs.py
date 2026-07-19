a4e27845f92400dbb1211855ce87c6b368fc1e5a
Tanzil Ehsan <tanzilehsan@gmail.com>
2026-07-19 17:45:55 -0400

    Add corrupted_chance function

--- generate_logs.py
+++ generate_logs.py
@@ -6,63 +6,45 @@ import time
 levels = ["INFO", "WARN", "ERROR"]
 
 
-
-# how to write json file in python (python file)
-# how to use datetime
-# dict = {timestamp: random(datetime), log-level: random(levels), status-code: random(http_status), response_time_ms: random(number generator?)  }
-# rand_timestamp = datetime.datetime.now().isoformat()
-
-# data = f" \"timestamp\": {generator_timestamp}, \"log_level\": {random.choice(levels)} \"status_code\": {random.choice(HTTP_Status)}, \"response_time_ms\" : {random.randint(10, 1000)}\n"
-
-
-# with open('server_logs.json', 'w') as f:
-#     for log in range(500):
-#         f.write(json.dumps(data, indent = 4))
-
-
-# with open("server_logs.json", "w") as f:
-#     for log in range(5):
-#         random_log_dict = {
-#             "timestamp": datetime.datetime.now().isoformat(),
-#             "log_level": random.choice(levels),
-#             "status_code": random.choice(HTTP_Status),
-#             "response_time_in_ms": random.randint(10, 1000),
-#         }
-#         f.write(json.dumps(random_log_dict))
-
-
-# with open("server_logs.json", "w") as f:
-#     all_logs=[]
-#     for log in range(5):
-#         random_log_dict = {
-#             "timestamp": datetime.datetime.now().isoformat(),
-#             "log_level": random.choice(levels),
-#             "status_code": random.choice(HTTP_Status),
-#             "response_time_in_ms": random.randint(10, 1000),
-#         }
-#         all_logs.append(random_log_dict)
-#     f.write(json.dumps(all_logs, indent=4))
+def corruption_chance():
+    if random.random() < 0.5:
+        corrupt_dict = {
+            "timestamp": datetime.datetime.now().isoformat(),
+            "log_level": random.choice(levels),
+            "status_code": random.choice(
+                ["200", "404", "500", "SUCCESS", "NO", "FAIL"]
+            ),
+            "response_time_in_ms": random.randint(10, 1000),
+        }
+        rand_corrupt_dict = random.sample(list(corrupt_dict.keys()), random.randint(1,4))
+        return {key: corrupt_dict[key] for key in rand_corrupt_dict}
 
 
 with open("server_logs.json", "w") as f:
-    all_logs=[]
-    for log in range(500):
+    all_logs = []
+    for log in range(10):
         random_log_dict = {
             "timestamp": datetime.datetime.now().isoformat(),
             "log_level": random.choice(levels),
             "status_code": 0,
             "response_time_in_ms": 0,
         }
-        if random_log_dict['log_level'] == "INFO":
-            random_log_dict['status_code'] = 200
-            random_log_dict['response_time_in_ms'] = random.randint(10, 150)
-        elif random_log_dict['log_level'] == "ERROR":
-            random_log_dict['status_code'] = 500
-            random_log_dict['response_time_in_ms'] = random.randint(400, 1000)
+        if random_log_dict["log_level"] == "INFO":
+            random_log_dict["status_code"] = 200
+            random_log_dict["response_time_in_ms"] = random.randint(10, 150)
+        elif random_log_dict["log_level"] == "ERROR":
+            random_log_dict["status_code"] = 500
+            random_log_dict["response_time_in_ms"] = random.randint(400, 1000)
         else:
-            random_log_dict['status_code'] = 404
-            random_log_dict['response_time_in_ms'] = random.randint(151, 399)
+            random_log_dict["status_code"] = 404
+            random_log_dict["response_time_in_ms"] = random.randint(151, 399)
+
+        get_corrupted = corruption_chance()
+
+        if get_corrupted:
+            all_logs.append(get_corrupted)
+        else:
+            all_logs.append(random_log_dict)
         
-        all_logs.append(random_log_dict)
-        time.sleep(0.5)
+        # time.sleep(0.5)
     f.write(json.dumps(all_logs, indent=4))
